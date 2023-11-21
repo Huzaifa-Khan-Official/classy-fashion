6f7f9e84fe86b77ca487449b65cf963af652bbaf
Huzaifa-Khan-Official <m.huzaifakhan2004@gmial.com>
2023-11-21 19:30:41 +0500

    working

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const homeFunc = () => {
         .then(res => res.json())
         .then(products => {
             containerDiv.innerHTML = "";
+            // remaing nav-items
+            womensClothing.classList.remove("active")
+            electronics.classList.remove("active")
+            jewelery.classList.remove("active")
+            mensClothing.classList.remove("active")
+            // active nav-item
+            home.classList.add("active");
             products.forEach(currentProduct => {
                 const productId = currentProduct.id;
                 const productTitle = currentProduct.title.slice(0, 50);
@@ -13,24 +20,42 @@ const homeFunc = () => {
                 const productImg = currentProduct.image;
                 const productDesc = currentProduct.description.slice(0, 100);
                 const productCategory = currentProduct.category;
-                const productRating = currentProduct.rating.rate;
+                const productRating = Math.ceil(currentProduct.rating.rate);
+                let ratingIcons;
+                if (productRating == 1) {
+                    ratingIcons = `<i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i>`
+                } else if (productRating == 2) {
+                    ratingIcons = `<i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i>`
+                } else if (productRating == 3) {
+                    ratingIcons = `<i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i>`
+                } else if (productRating == 4) {
+                    ratingIcons = `<i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i>`
+                } else if (productRating == 5) {
+                    ratingIcons = `<i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i><i class="fa-solid fa-star" style="color: #18C8A8;"></i>`
+                } else {
+                    ratingIcons = `<i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i><i class="fa-regular fa-star" style="color: #18C8A8;"></i>`
+                }
                 const productRatingCount = currentProduct.rating.count;
                 containerDiv.innerHTML += `
-            <div class="card">
-                <img src="${productImg}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <div class="titleDiv">                    
-                        <h6 class="card-title">${productTitle}</h6>
+                    <div class="card">
+                        <img src="${productImg}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class="titleDiv">                    
+                                <h6 class="card-title">${productTitle}</h6>
+                            </div>
+                            <div>
+                                <h5 class="card-title">$${productPrice}</h5>
+                            </div>
+                            <div class="descDiv">                    
+                                <p class="card-text">${productDesc}...</p>
+                            </div>
+                            <div><p>${ratingIcons} (${productRatingCount})</p></div>
+                            <div class="btnDiv mt-3">                    
+                                <a href="#" class="btn btn-primary">Add to chart</a>
+                            </div>
+                        </div>
                     </div>
-                    <div class="descDiv">                    
-                        <p class="card-text">${productDesc}...</p>
-                    </div>
-                    <div class="btnDiv mt-3">                    
-                        <a href="#" class="btn btn-primary">Add to chart</a>
-                    </div>
-                </div>
-            </div>
-            `
+                `
             });
         })
         .catch(err => console.log(err));
@@ -53,7 +78,13 @@ electronics.addEventListener("click", () => {
         .then(res => res.json())
         .then(products => {
             containerDiv.innerHTML = "";
-            // console.log(electronics);
+            // remaing nav-items
+            home.classList.remove("active")
+            jewelery.classList.remove("active")
+            mensClothing.classList.remove("active")
+            womensClothing.classList.remove("active")
+            // active nav-item
+            electronics.classList.add("active");
             products.forEach(currentProduct => {
                 const productId = currentProduct.id;
                 const productTitle = currentProduct.title.slice(0, 50);
@@ -69,6 +100,9 @@ electronics.addEventListener("click", () => {
                 <div class="card-body">
                     <div class="titleDiv">                    
                         <h6 class="card-title">${productTitle}</h6>
+                    </div>
+                    <div>
+                        <h5 class="card-title">$${productPrice}</h5>
                     </div>
                     <div class="descDiv">                    
                         <p class="card-text">${productDesc}...</p>
@@ -89,6 +123,13 @@ jewelery.addEventListener("click", () => {
         .then(res => res.json())
         .then(products => {
             containerDiv.innerHTML = "";
+            // remaing nav-items
+            home.classList.remove("active")
+            electronics.classList.remove("active")
+            mensClothing.classList.remove("active")
+            womensClothing.classList.remove("active")
+            // active nav-item
+            jewelery.classList.add("active");
             products.forEach(currentProduct => {
                 const productId = currentProduct.id;
                 const productTitle = currentProduct.title.slice(0, 50);
@@ -104,6 +145,9 @@ jewelery.addEventListener("click", () => {
                 <div class="card-body">
                     <div class="titleDiv">                    
                         <h6 class="card-title">${productTitle}</h6>
+                    </div>
+                    <div>
+                        <h5 class="card-title">$${productPrice}</h5>
                     </div>
                     <div class="descDiv">                    
                         <p class="card-text">${productDesc}...</p>
@@ -124,6 +168,13 @@ mensClothing.addEventListener("click", () => {
         .then(res => res.json())
         .then(products => {
             containerDiv.innerHTML = "";
+            // remaing nav-items
+            home.classList.remove("active")
+            electronics.classList.remove("active")
+            jewelery.classList.remove("active")
+            womensClothing.classList.remove("active")
+            // active nav-item
+            mensClothing.classList.add("active");
             products.forEach(currentProduct => {
                 const productId = currentProduct.id;
                 const productTitle = currentProduct.title.slice(0, 50);
@@ -143,6 +194,9 @@ mensClothing.addEventListener("click", () => {
                     <div class="descDiv">                    
                         <p class="card-text">${productDesc}...</p>
                     </div>
+                    <div>
+                        <h5 class="card-title">$${productPrice}</h5>
+                    </div>
                     <div class="btnDiv mt-3">                    
                         <a href="#" class="btn btn-primary">Add to chart</a>
                     </div>
@@ -159,6 +213,13 @@ womensClothing.addEventListener("click", () => {
         .then(res => res.json())
         .then(products => {
             containerDiv.innerHTML = "";
+            // remaing nav-items
+            home.classList.remove("active")
+            electronics.classList.remove("active")
+            jewelery.classList.remove("active")
+            mensClothing.classList.remove("active")
+            // active nav-item
+            womensClothing.classList.add("active");
             products.forEach(currentProduct => {
                 const productId = currentProduct.id;
                 const productTitle = currentProduct.title.slice(0, 50);
@@ -174,6 +235,9 @@ womensClothing.addEventListener("click", () => {
                 <div class="card-body">
                     <div class="titleDiv">                    
                         <h6 class="card-title">${productTitle}</h6>
+                    </div>
+                    <div>
+                        <h5 class="card-title">$${productPrice}</h5>
                     </div>
                     <div class="descDiv">                    
                         <p class="card-text">${productDesc}...</p>
